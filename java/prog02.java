95faccaba6b823da4b0501dcfb31e7346b003667
Bhavesh Gyanchandani <bhaveshgyanchandanip1@example.com>
2024-07-23 16:18:12 +0530

    new3

---
+++ java/prog02.java
@@ -0,0 +1,14 @@
+import  java.util.Scanner;
+
+public class prog02 {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+
+            sc.close();
+    }
+}
